0a93ecd37670d787564641badc10b6b902e99817
BrightSprinkler <spammeiemmaps@gmail.com>
2021-08-31 17:43:43 +0200

    bugfix

--- SnakesAndLadderWheelProbabilities/Program.cs
+++ SnakesAndLadderWheelProbabilities/Program.cs
@@ -8,12 +8,7 @@ namespace SnakesAndLadderWheelProbabilities
     {
         static void Main(string[] args)
         {
-            Roll(1, 4, 100000, "D4");
             Roll(1, 6, 100000, "D6");
-            Roll(1, 8, 100000, "D8");
-            Roll(1, 10, 100000, "D10");
-            Roll(1, 12, 100000, "D12");
-            Roll(1, 20, 100000, "D20");
         }
 
         private static void Roll(int min, int max, int gameCount, string dice)
@@ -34,13 +29,15 @@ namespace SnakesAndLadderWheelProbabilities
             Console.WriteLine($"Completed after min {rolls.Min()} rolls.");
             Console.WriteLine($"Completed after max {rolls.Max()} rolls.");
             Console.WriteLine();
+            for (int i = 0; i < 100; i += 10) PrintRangeData(i, i + 10, rolls);
+            Console.WriteLine();
             for (int i = 0; i <= 1000; i += 100) PrintRangeData(i, i + 100, rolls);
             Console.WriteLine();
         }
 
         private static void PrintRangeData(int min, int max, List<int> rolls)
         {
-            int count = rolls.Where(x => x > min && x < max).Count();
+            int count = rolls.Where(x => x >= min && x < max).Count();
 
             Console.WriteLine($"Games between {min} and {max} Rolls: {count} - {((decimal)count / (decimal)rolls.Count()) * 100} %");
         }
